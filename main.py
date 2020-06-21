2d450c3c2b17b97d8d778bb5dc7828e0db99ad1d
Falah Naufal Zaki <falah.naufal.zk@gmail.com>
2020-06-21 20:04:33 +0700

    6 commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/', methods = ['POST', 'GET'])
 def home():
-    return render_template("homePage.html")
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
