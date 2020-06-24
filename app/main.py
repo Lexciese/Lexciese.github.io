6017effe231894b722746a1167099dd03bf2e50c
Falah Naufal Zaki <falah.naufal.zk@gmail.com>
2020-06-24 18:33:30 +0700

    14 commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,44 @@
 from flask import Flask, render_template, request
+import requests
 
+app = Flask(__name__)
 
+URL = "https://geocode.search.hereapi.com/v1/geocode"
+location = "yogyakarta" #taking user input
+api_key = 'GVIG0IOWHYL1R8m9a3KL9-dA9PYlAbLFwE5RLrVeiGg' # Acquire from developer.here.com
+PARAMS = {'apikey':api_key,'q':location} 
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+# sending get request and saving the response as response object 
+r = requests.get(url = URL, params = PARAMS) 
+data = r.json()
+#print(data)
 
+#Acquiring the latitude and longitude from JSON 
+latitude = data['items'][0]['position']['lat']
+print(latitude)
+longitude = data['items'][0]['position']['lng']
+print(longitude)
 
+api_weather = "64bb6bc59054bb41eb4db1bc5154eb76"
+getWeather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat=" + str(latitude) + "&lon=" + str(longitude) + "&appid=" + api_weather + '&units=metric')
+weather = getWeather.json()
+
+cuaca = weather['weather'][0]['main']
+ketCuaca = weather['weather'][0]['description']
+icon = weather['weather'][0]['icon']
+suhu = weather['main']['temp']
+feels_like = weather['main']['feels_like']
+pressure = weather['main']['pressure']
+humidity = weather['main']['humidity']
+wind = weather['wind']['speed']
+print(weather)
+print(cuaca)
+print(icon)
 
 @app.route('/home')
 @app.route('/', methods = ['POST', 'GET'])
 def home():
-    return render_template("index.html")
+    data = {'weather_raw': weather, 'cuaca': cuaca, 'ketCuaca': ketCuaca, 'icon': icon,'suhu': suhu,'feels_like': feels_like,'pressure': pressure,'humidity': humidity,'wind': wind}
+    return render_template("index.html", data=data)
+
+
--- app/static/homePage.js
+++ app/static/homePage.js
@@ -1,5 +1,3 @@
-let txt1 = document.getElementById("txt1");
-
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -12,7 +10,7 @@ function startTime() {
 }
 
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
     return i;
 }
 
@@ -29,38 +27,48 @@ var pagiSiangMalam = new Date();
 var checkH = pagiSiangMalam.getHours();
 console.log(checkH)
 if (checkH >= 0 && checkH <= 11) {
-    document.getElementById('txt2').innerHTML = "Selamat Pagi,<br>Falah Naufal Zaki";
+    document.getElementById('txt2').innerHTML = "Selamat Pagi,<br>";
     document.body.style.backgroundImage = "url('/static/assets/images/sunrise.jpg')"
 
-}else if (checkH >=12 && checkH <= 15) {
-    document.getElementById('txt2').innerHTML = "Selamat Siang,<br>Falah Naufal Zaki";
+} else if (checkH >= 12 && checkH <= 15) {
+    document.getElementById('txt2').innerHTML = "Selamat Siang,<br>";
     document.body.style.backgroundImage = "url(https://lh6.googleusercontent.com/proxy/g3MNRR4S0VBk1KWmTem8W_eoNZkjk4v5AKsQ4tUYpp4_ISwDFXdLwYQVPklvs0FjC6gGCbi1TqTgB3YNS6qSV80X3LozHJYmmHImtTqinxPl=w3840-h2160-p-k-no-nd-mv)";
-    txtColorToBlack;
+    txtColorToBlack();
 
-}else if (checkH >=16 && checkH <= 17) {
-    document.getElementById('txt2').innerHTML = "Selamat Sore,<br>Falah Naufal Zaki";
+} else if (checkH >= 16 && checkH <= 17) {
+    document.getElementById('txt2').innerHTML = "Selamat Sore,<br>";
     document.body.style.backgroundImage = "url('/static/assets/images/sunset.jpg')"
 
-}else {
-    document.getElementById('txt2').innerHTML = "Selamat Malam,<br>Falah Naufal Zaki";
+} else {
+    document.getElementById('txt2').innerHTML = "Selamat Malam,<br>";
     document.body.style.backgroundImage = "url('/static/assets/images/night.jpg')"
-    
-    
+
+
 }
 
 
 var input = document.getElementById("input");
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
+input.addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      document.getElementById("btn").click();
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btn").click();
     }
-  });
+});
+
+
+
+// function showCuaca() {
+//     var icon = document.getElementById("icon");
+//     icon.addEventListener("click", function() {
+
+//     })
+// }
+
 
 
 
